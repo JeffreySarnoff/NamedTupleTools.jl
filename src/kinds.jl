e947bc3f58738ed61fee1aefac3ea355a0180894
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-13 08:01:19 -0500

    Create kinds.jl

---
+++ src/kinds.jl
@@ -0,0 +1,34 @@
+#=
+    On The Kinds of NamedTuple
+=#
+
+const AkoNamedTuple = Val{:AkoNamedTuple}
+
+const AbstractNamedTuple = :AbstractNamedTuple
+const RealizedNamedTuple = :RealizedNamedTuple
+
+isa(x, y) = Base.isa(x, y)
+
+isa(x, ::Type{AkoNamedTuple}) = false
+
+isa(x::NamedTuple{T}, ::Type{AkoNamedTuple}) where T = true
+isa(::Type{NamedTuple{T}}, ::Type{AkoNamedTuple}) where T = true
+
+
+#=
+thesenames = (:a, :b, :c)
+thesevalues = ('a', 'b', 'c')
+prototype_nt = NamedTuple{thesenames}
+realized_nt = prototype_nt(thesevalues)
+whatami(prototype_nt) = :prototype_nt
+whatami(realized_nt) = :realized_nt
+@inline whatami(ako_nt) = isa(ako_nt, Type)
+isa(prototype_nt,Type) == true
+isa(realized_nt,Type) == false
+akoNamedTuple(x) = isa(Base._nt_names(realized_nt), Tuple)
+isa( realized_nt, NamedTuple ) == true
+isa( prototype_nt, NamedTuple ) == false
+akoNamedTuple(x) = false
+akoNamedTuple(::Type{NamedTuple{T}}) where T = true
+akoNamedTuple(x::NamedTuple{T}) where T = true
+=#
