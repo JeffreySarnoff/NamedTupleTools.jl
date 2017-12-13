87276e416911d6937c65157500813bfa56ae7330
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-13 06:38:21 -0500

    names, values

--- src/parts.jl
+++ src/parts.jl
@@ -1,5 +1,32 @@
 #=
 
-    Parts with NamedTuples
+    NamedTuples as the Parts thereof
 
 =#
+
+
+"""
+    NT_names(namedtuple)
+
+Obtains the names with which `namedtuple` labels values.
+
+#Example
+
+nt = NamedTuple{(:first, :second)}(("initial", "final"))
+NT_names(nt) == (:first, :second)
+
+"""
+NT_names(namedtuple::T) where T<:NamedTuple = Base._nt_names(namedtuple)
+
+"""
+    NT_values(namedtuple)
+
+Obtains the values that `namedtuple` labels with the names.
+
+#Example
+
+nt = NamedTuple{(:first, :second)}(("initial", "final"))
+NT_values(nt) == ("initial", "final")
+
+"""
+NT_values(namedtuple::T) where T<:NamedTuple = Base.values(namedtuple)
