b59b356ced7a67c4afc132d5e5bf10bcfb75a44b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-13 06:53:41 -0500

    namesvalues

--- src/parts.jl
+++ src/parts.jl
@@ -19,6 +19,18 @@ NT_names(nt) == (:first, :second)
 NT_names(namedtuple::T) where T<:NamedTuple = Base._nt_names(namedtuple)
 
 """
+    NT_names(namedtuple, ordinal)
+    NT_names(namedtuple, unitrange)
+    NT_names(namedtuple, ordinals)
+
+Obtains one or more symbols from the full sequence of names
+"""
+NT_names(namedtuple, idx::I) where I<:SIUI = NT_names(namedtuple)[idx]
+NT_names(namedtuple, idxs::R) where R<:UnitRange = NT_names(namedtuple)[idxs]
+NT_names(namedtuple, idxs::T) where T<:Tuple = NT_names(namedtuple)[idxs...,]
+
+
+"""
     NT_values(namedtuple)
 
 Obtains the values that `namedtuple` labels with the names.
@@ -30,3 +42,39 @@ NT_values(nt) == ("initial", "final")
 
 """
 NT_values(namedtuple::T) where T<:NamedTuple = Base.values(namedtuple)
+
+"""
+    NT_values(namedtuple, ordinal)
+    NT_values(namedtuple, unitrange)
+    NT_values(namedtuple, ordinals)
+
+Obtains one or more realizations from the full sequence of values
+"""
+NT_values(namedtuple, idx::I) where I<:SIUI = NT_values(namedtuple)[idx]
+NT_values(namedtuple, idxs::R) where R<:UnitRange = NT_values(namedtuple)[idxs]
+NT_values(namedtuple, idxs::T) where T<:Tuple = NT_values(namedtuple)[idxs...,]
+
+"""
+    NT_namesvalues(namedtuple)
+
+Obtains the names and the values with which `namedtuple` is realized.
+
+#Example
+
+nt = NamedTuple{(:first, :second)}(("initial", "final"))
+NT_namesvalues(nt) == ((:first, :second), ("initial", "final"))
+
+"""
+NT_namesvalues(namedtuple::T) where T<:NamedTuple = (names(namedtuple), values(namedtuple))
+
+"""
+    NT_namesvalues(namedtuple, ordinal)
+    NT_namesvalues(namedtuple, unitrange)
+    NT_namesvalues(namedtuple, ordinals)
+
+Obtains one or more of the names and values with which `namedtuple` is realized.
+"""
+NT_namesvalues(namedtuple, idx::I) where I<:SIUI = (NT_names(namedtuple)[idx], NT_values(namedtuple)[idx])
+NT_namesvalues(namedtuple, idxs::R) where R<:UnitRange = (NT_names(namedtuple)[idxs], NT_values(namedtuple)[idxs])
+NT_namesvalues(namedtuple, idxs::T) where T<:Tuple = (NT_names(namedtuple)[idxs...,], NT_values(namedtuple)[idxs...,])
+
