5df091b4d021d93bdfc1570d162eb2e11d5dc36d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-14 00:06:21 -0500

    Update kinds.jl

--- src/kinds.jl
+++ src/kinds.jl
@@ -5,12 +5,13 @@
 isako(x, y) = Base.is(x, y)
 
 const KindsOfNamedTuples = Union{NamedTuple, NamedTuple{N}} where N
-const AbstractNamedTuple = Val{:AbstractNamedTuple}
-const RealizedNamedTuple = Val{:RealizedNamedTuple}
 
 isako(x, NamedTuple) = x <: KindsOfNamedTuples
+isako(x, ::Type{NamedTuple{T}}) where T = isa(x, NamedTuple) & isa(x, Type)
+isako(x::NamedTuple{T}, ::TypeaVBNamedTuple}) where T = isa(x, Type)
+isako(x::NamedTuple{T}, ::Type{RealizedNamedTuple}) where T = isa(x, Type)
 
-isako(::Type{NamedTuple{T}}, ::Type{AbstractNamedTuple}) = true
-isako(::Type{NamedTuple{T}}, ::Type{RealizedNamedTuple}) = false
+isako(::Type{NamedTuple{T}}, ::Type{AbstractNamedTuple}) where T = true
+isako(::Type{NamedTuple{T}}, ::Type{RealizedNamedTuple}) where T = false
 isako(x::NamedTuple{T}, ::Type{RealizedNamedTuple}) where T = true
 isako(x::NamedTuple{T}, ::Type{AbstractNamedTuple}) where T = false
