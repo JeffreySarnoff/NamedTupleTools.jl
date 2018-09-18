c86983bdb7f3976be3c91cc01128cbe136faddb9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-18 04:12:39 -0400

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -3,40 +3,40 @@
 
 This module provides some useful NamedTuple tooling.
 
-@ref(ntproto), @ref(ntnames), @ref(ntvalues)
+@ref(tuplenames), @ref(fieldnames), @ref(values)
 """
 module NamedTupleTools
 
-export ntproto, ntnames, ntvalues
+export tuplenames
 
 # accept comma delimited values
 Base.NamedTuple{T}(xs...) where {T} = NamedTuple{T}(xs)
 
 """
-    ntproto(  name1, name2, ..  )
-    ntproto( (name1, name2, ..) )
+    tuplenames(  name1, name2, ..  )
+    tuplenames( (name1, name2, ..) )
 
 Generate a NamedTuple prototype by specifying the field names.
 The prototype is applied to field values, giving a completed NamedTuple.
 """
-ntproto(names::NTuple{N,Symbol}) where {N} = NamedTuple{names}
-ntproto(names::Vararg{Symbol}) = NamedTuple{names}
+tuplenames(names::NTuple{N,Symbol}) where {N} = NamedTuple{names}
+tuplenames(names::Vararg{Symbol}) = NamedTuple{names}
 
 """
-    ntnames( ntprototype )
-    ntnames( namedtuple  )
+    fieldnames( ntprototype )
+    fieldnames( namedtuple  )
 
 Retrieve the names as a tuple of symbols.
 """
-ntnames(::Type{T}) where {T<:NamedTuple} = Base._nt_names(T)
-ntnames(nt::T) where {T<:NamedTuple} = Base._nt_names(T)
+Base.fieldnames(::Type{T}) where {T<:NamedTuple} = Base._nt_names(T)
+Base.fieldnames(nt::T) where {T<:NamedTuple} = Base._nt_names(T)
 
 """
-    ntvalues( namedtuple )
+    values( namedtuple )
 
 Retrieve the values as a tuple.
 """
-ntvalues(::Type{T}) where {T<:NamedTuple} = ()
-ntvalues(nt::T) where {T<:NamedTuple} = Base.values(nt)
+Base.values(::Type{T}) where {T<:NamedTuple} = ()
+Base.values(nt::T) where {T<:NamedTuple} = Base.values(nt)
 
 end # module NamedTupleTools
