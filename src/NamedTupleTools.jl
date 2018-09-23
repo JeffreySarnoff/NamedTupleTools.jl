97fd289a89c78be1129db89fa2b5eb68e70ed0d9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-23 03:36:19 -0400

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -27,6 +27,20 @@ length(::Type{T}) where {N,T<:NamedTuple{N}} = length(N)
 Generate a NamedTuple prototype by specifying or obtaining the fieldnames.
 The prototype is applied to fieldvalues, giving a completed NamedTuple.
 
+# Example
+
+julia> ntprototype = namedtuple( :a, :b, :c )
+
+NamedTuple{(:a, :b, :c),T} where T<:Tuple
+
+julia> nt123 = ntprototype(1, 2, 3)
+
+(a = 1, b = 2, c = 3)
+
+julia> ntAb3 = ntprototype("A", "b", 3)
+
+(a = "A", b = "b", c = 3)
+
 see: [`isprototype`](@ref)
 """
 namedtuple(names::NTuple{N,Symbol}) where {N} = NamedTuple{names}
