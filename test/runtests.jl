ced9a9f8aaaaf18383688f74e5f3c95ff11f2750
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-10-11 10:14:58 -0400

    delete! -> delete

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,8 +15,8 @@ nt2 = ntproto2("one", "two")
 @test nt2 === (a = "one", b = "two")
 @test ntproto2(nt1) === (a = 1, b = 2)
 
-@test delete!(ntproto1, :a) === NamedTuple{(:b, :c, :d),T} where T<:Tuple
-@test delete!(ntproto1, :a, :c) === NamedTuple{(:b, :d),T} where T<:Tuple
-@test delete!(ntproto1, (:b, :c)) === NamedTuple{(:a, :d),T} where T<:Tuple
+@test delete(ntproto1, :a) === NamedTuple{(:b, :c, :d),T} where T<:Tuple
+@test delete(ntproto1, :a, :c) === NamedTuple{(:b, :d),T} where T<:Tuple
+@test delete(ntproto1, (:b, :c)) === NamedTuple{(:a, :d),T} where T<:Tuple
 
 @test merge(nt1, nt2) === (a = "one", b  = "two", c = 3, d = 4)
