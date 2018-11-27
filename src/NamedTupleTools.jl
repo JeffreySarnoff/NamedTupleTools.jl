4cd8f3963a1d2c97e5e6dd47ee54bd1e8f7922a9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-26 22:08:05 -0500

    merge( 2..7 )

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -170,6 +170,8 @@ merge(::Type{T1}, ::Type{T2}, ::Type{T3}, ::Type{T4}, ::Type{T5}) where {N1,N2,N
     namedtuple((unique((N1..., N2..., N3..., N4..., N5...,))...,))
 merge(::Type{T1}, ::Type{T2}, ::Type{T3}, ::Type{T4}, ::Type{T5}, ::Type{T6}) where {N1,N2,N3,N4,N5,N6,T1<:NamedTuple{N1},T2<:NamedTuple{N2},T3<:NamedTuple{N3},T4<:NamedTuple{N4},T5<:NamedTuple{N5},T6<:NamedTuple{N6}} =
     namedtuple((unique((N1..., N2..., N3..., N4..., N5..., N6...,))...,))
+merge(::Type{T1}, ::Type{T2}, ::Type{T3}, ::Type{T4}, ::Type{T5}, ::Type{T6}, ::Type{T7}) where {N1,N2,N3,N4,N5,N6,N7,T1<:NamedTuple{N1},T2<:NamedTuple{N2},T3<:NamedTuple{N3},T4<:NamedTuple{N4},T5<:NamedTuple{N5},T6<:NamedTuple{N6},T7<:NamedTuple{N7}} =
+    namedtuple((unique((N1..., N2..., N3..., N4..., N5..., N6...,N7...))...,))
 
 # merge(nt1::T1, nt2::T2) where {T1<:NamedTuple, T2<:NamedTuple} is already defined
 
@@ -181,6 +183,8 @@ merge(a::NamedTuple{an}, b::NamedTuple{bn}, c::NamedTuple{cn}, d::NamedTuple{dn}
     reduce(merge,(a, b, c, d, e))
 merge(a::NamedTuple{an}, b::NamedTuple{bn}, c::NamedTuple{cn}, d::NamedTuple{dn}, e::NamedTuple{en}, f::NamedTuple{fn}) where {an, bn, cn, dn, en, fn} =
     reduce(merge,(a, b, c, d, e, f))
+merge(a::NamedTuple{an}, b::NamedTuple{bn}, c::NamedTuple{cn}, d::NamedTuple{dn}, e::NamedTuple{en}, f::NamedTuple{fn}, g::NamedTuple{gn}) where {an, bn, cn, dn, en, fn, gn} =
+    reduce(merge,(a, b, c, d, e, f, g))
 
 
 # conversions
