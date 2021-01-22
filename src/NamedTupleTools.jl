4bb99f85ae7d580c6011e1d1eaf826c7c3586aa7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-01-22 10:25:41 -0500

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -516,19 +516,19 @@ Collect the elements of x into a Tuple, in their iterated order.
 """
 @inline gather_(x::T) where {T} = (collect(x)...,)
 
-if VERSION >= v"1.5.9"
+if VERSION > v"1.5.9"
  	namedtuple(d::T) where {T<:AbstractDict{Symbol,V}} where {V} =
 		NamedTuple(d)
 else
 	namedtuple(d::T) where {T<:AbstractDict{Symbol,V}} where {V} =
 	    NamedTuple{gather_(keys(d)), NTuple{length(d), V}}(gather_(values(d)))
-	namedtuple(d::T) where {T<:AbstractDict{S,V}} where {S<:AbstractString, V} =
-	    NamedTuple{Symbol.(gather_(keys(d))), NTuple{length(d), V}}(gather_(values(d)))
-	namedtuple(d::T) where {T<:AbstractDict{Symbol,Any}} =
-	    NamedTuple{gather_(keys(d)), Tuple{typeof.(values(d))...}}(gather_(values(d)))
-	namedtuple(d::T) where {T<:AbstractDict{S,Any}} where {S<:AbstractString} =
-	    NamedTuple{Symbol.(gather_(keys(d))), Tuple{typeof.(values(d))...}}(gather_(values(d)))
-end
+end		
+namedtuple(d::T) where {T<:AbstractDict{S,V}} where {S<:AbstractString, V} =
+    NamedTuple{Symbol.(gather_(keys(d))), NTuple{length(d), V}}(gather_(values(d)))
+namedtuple(d::T) where {T<:AbstractDict{Symbol,Any}} =
+    NamedTuple{gather_(keys(d)), Tuple{typeof.(values(d))...}}(gather_(values(d)))
+namedtuple(d::T) where {T<:AbstractDict{S,Any}} where {S<:AbstractString} =
+    NamedTuple{Symbol.(gather_(keys(d))), Tuple{typeof.(values(d))...}}(gather_(values(d)))
 	
 # use: dict = convert(Dict, nt)
 #=
